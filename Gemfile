0a893077a3db472dedb6ff00bd361760197f3c63
Jason Janson <rookerith@gmail.com>
2013-05-16 16:55:41 -0400

    Started cucumber rspec training
    Just my book examples.

---
+++ Gemfile
@@ -0,0 +1,27 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.13'
+gem 'bootstrap-sass'
+gem 'haml-rails'
+gem 'jquery-rails'
+
+group :assets do
+	gem 'sass-rails',   '~> 3.2.3'
+	gem 'coffee-rails', '~> 3.2.1'
+	gem 'uglifier', '>= 1.0.3'
+end
+
+group :test do
+	gem 'cucumber-rails'
+	gem 'capybara'
+	gem 'database_cleaner'
+end
+
+group :development, :test do
+	gem 'rspec-rails'
+	gem 'sqlite3'
+end
+
+group :production do
+	gem 'mysql2'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BullsCows::Application.config.secret_token = 'bee8dac5caa0ea6d826ec90bfefe6d4cb147c94b42fe3a535fff8cd942f23c06109fbe8b6a5a95b0f8e270f9fb344e8f9f9cf4adb543245e06a89699352eb8c8'
